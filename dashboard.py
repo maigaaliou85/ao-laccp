bb1dc8e81e2c043adb8790d2d5a3db28b5d9ccd8
maigaaliou85 <44571492+maigaaliou85@users.noreply.github.com>
2026-09-13 23:29:48 -0400

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -113,15 +113,7 @@ def run_module(folder_name):
     else:
         st.error(f"Module not found: {module_path}")
 
-def run_module(folder_name):
-  module_path = os.path.join(folder_name, "main33.py")
-    if os.path.exists(module_path):
-        spec = importlib.util.spec_from_file_location("module_main", module_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules["module_main"] = module
-        spec.loader.exec_module(module)
-    else:
-        st.error(f"Module not found: {module_path}")
+
 
 # ==============================
 # Run Title Page or Module
